ef522565a0230dfa6138b695f7cc965414227961
kuxidaica <hth0901@gmail.com>
2019-09-17 07:35:16 +0700

    ~~

--- AdminTool/app/components/semester/semesterAddController.js
+++ AdminTool/app/components/semester/semesterAddController.js
@@ -4,12 +4,15 @@
     semesterAddController.$inject = ['$scope', 'apiService'];
 
     function semesterAddController($scope, apiService) {
-        $scope.eSemeter = {
-            
-        };
+        $scope.eSemeter = {};
 
         $scope.addNewSemester = function () {
             console.log($scope.eSemeter);
+            apiService.post('http://localhost:3433/api/semester/createnew', $scope.eSemeter, function (result) {
+                console.log(result);
+            }, function (error) {
+                console.log(error);
+            });
         }
     }
 })(angular.module('admintool.semester'));
