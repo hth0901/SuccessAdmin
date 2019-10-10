fd2d6860666c8e5676fe6a32c8f9bd55935c8520
kuxidaica <hth0901@gmail.com>
2019-10-10 07:35:02 +0700

    ~~

--- AdminTool/app/components/semester/semesterEditController.js
+++ AdminTool/app/components/semester/semesterEditController.js
@@ -4,6 +4,22 @@
     semesterEditController.$inject = ['$scope', 'apiService'];
 
     function semesterEditController($scope, apiService) {
+        $scope.eSemeter = {};
 
+        $scope.getSemester = function () {
+            console.log($scope.eSemeter);
+            let config = {
+                params: {
+                    semesterCode: '123123'
+                }
+            }
+            apiService.get('http://localhost:3433/api/semester/getdetail', config, function (result) {
+                console.log(result);
+            }, function (error) {
+                console.log(error);
+            });
+        }
+
+        $scope.getSemester();
     }
 })(angular.module('admintool.semester'));
--- AdminTool/app/components/semester/semesterListController.js
+++ AdminTool/app/components/semester/semesterListController.js
@@ -4,6 +4,16 @@
     semesterListController.$inject = ['$scope', 'apiService'];
 
     function semesterListController($scope, apiService) {
+        $scope.lstSemester = [];
 
+        $scope.getAllSemester = function () {
+            apiService.get('http://localhost:3433/api/semester/getallsemester', null, function (result) {
+                console.log(result);
+            }, function (error) {
+                console.log(error);
+            });
+        }
+
+        $scope.getAllSemester();
     }
 })(angular.module('admintool.semester'));
